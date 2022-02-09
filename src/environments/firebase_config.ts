77a69a9d795063e218c728149f4b6a024dddc078
Serban Tudor Gabriel <tudor.gabriel.serban@gmail.com>
2022-02-09 21:37:44 +0200

    0.0.4 Capacitor integrations

---
+++ src/environments/firebase_config.ts
@@ -0,0 +1,13 @@
+
+
+export const firebase= {
+    projectId: 'partymusicrequest',
+    appId: '1:488824036938:web:1ff8cd4f9b4b7fff2f10b3',
+    storageBucket: 'partymusicrequest.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyARDu8MlVdnb9H63eyw2EJTuj2jb2JQgHc',
+    authDomain: 'partymusicrequest.firebaseapp.com',
+    messagingSenderId: '488824036938',
+    measurementId: 'G-CJETFQFS1E',
+ 
+  }
